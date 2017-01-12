bc297daf3167115fb7a00a3de88b2166cefaa7f7
Aaron <supermanaaronliu@gmail.com>
2017-01-11 18:05:07 -0700

    step 23;

--- script.py
+++ script.py
@@ -4,7 +4,7 @@ import requests
 
 print requests.__version__
 
-response = requests.get("http://google.com/")
+response = requests.get("https://raw.githubusercontent.com/AaronHongyangLiu/c404LAB/master/script.py")
 
 print response.text
 print response.staus_code
